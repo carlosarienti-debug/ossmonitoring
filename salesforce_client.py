3450b87703bd16e29e08412767d3aa00c9c46fc9
Arienti <EB7KAAB@vw.vwgroup.com>
2026-04-27 21:26:57 -0300

    fix: remove hardcoded instance_url, use domain param para login SF

--- salesforce_client.py
+++ salesforce_client.py
@@ -8,7 +8,7 @@ def get_sf_client() -> Salesforce:
         username=os.environ["SF_USERNAME"],
         password=os.environ["SF_PASSWORD"],
         security_token=os.environ["SF_SECURITY_TOKEN"],
-        instance_url="https://crmvw.my.salesforce.com",
+        domain=os.environ.get("SF_DOMAIN", "login"),  # use 'test' for sandbox
     )
 
 
